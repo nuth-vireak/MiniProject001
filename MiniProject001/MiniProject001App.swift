4bcc9aa0b7b7b413cfbb7ac61643c2df616d462a
nuth-vireak <timberr.4t@gmail.com>
2024-09-18 10:41:33 +0700

    Initial Commit

---
+++ MiniProject001/MiniProject001App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniProject001App.swift
+//  MiniProject001
+//
+//  Created by KAK-REAK on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniProject001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
